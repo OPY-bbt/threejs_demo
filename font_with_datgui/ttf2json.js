67fa118d67d1df484ce6c1f32165885ca09bf703
张岩 <zhyabs@gmail.com>
2020-01-03 12:20:34 +0800

    fix: font glyph missing

--- font_with_datgui/ttf2json.js
+++ font_with_datgui/ttf2json.js
@@ -39,7 +39,7 @@ var convert = function(font, text){
                 needToExport = (restrictCharacterSetInput.value.indexOf (glyphCharacter) != -1);
             }
             if (needToExport) {
-
+                console.log(glyph.index);
                 var token = {};
                 token.ha = Math.round(glyph.advanceWidth * scale);
                 token.x_min = Math.round(glyph.xMin * scale);
